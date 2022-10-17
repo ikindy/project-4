d555d3ceda8cfecf6882ba0a40d3764129240e75
kindheart <kindyilegogie@gmail.com>
2022-10-17 15:38:00 +0100

    kindy

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'edsr3cxif3';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-t4bzf4yc.us.auth0.com',
+  clientId: 'zS7voIUq0fvhZWJloSqNqUeZvq5415ZV',
+  callbackUrl: 'http://localhost:3000/callback'
+};
